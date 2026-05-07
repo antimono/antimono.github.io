701943fe174f79f02124d8d2ccdd679da3e16f1f
Oskar Hahn <xonoma67@gmail.com>
2026-05-07 20:06:08 +1000

    created loan dashboard

---
+++ loan-dashboard/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA4Wsiu0k-eb_Dx4aglVhqXntD9Wmhng-U",
+    authDomain: "orcabank.firebaseapp.com",
+    projectId: "orcabank",
+    storageBucket: "orcabank.firebasestorage.app",
+    messagingSenderId: "225845319467",
+    appId: "1:225845319467:web:5dec3379b4bc574bd0d76e"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
